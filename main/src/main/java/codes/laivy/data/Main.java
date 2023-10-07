b40bfa4d59db3ffd524929848c1929c499a32331
Laivy <dnlfg.contato@gmail.com>
2023-10-07 15:36:16 -0300

    Added modules

---
+++ main/src/main/java/codes/laivy/data/Main.java
@@ -0,0 +1,12 @@
+package codes.laivy.data;
+
+public final class Main {
+
+    private Main() {
+        throw new UnsupportedOperationException("This class cannot be instantiated");
+    }
+
+    public static void main(String[] args) {
+        // Nothing here
+    }
+}
