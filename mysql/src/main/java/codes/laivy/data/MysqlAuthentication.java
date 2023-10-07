7c2d44586d41d3e2cd37b9b3e8508d2b9db80cf4
Laivy <dnlfg.contato@gmail.com>
2023-10-07 17:10:47 -0300

    Added mysql database and authentication system

---
+++ mysql/src/main/java/codes/laivy/data/MysqlAuthentication.java
@@ -0,0 +1,207 @@
+package codes.laivy.data;
+
+import org.jetbrains.annotations.*;
+
+import java.net.InetAddress;
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class MysqlAuthentication {
+
+    private final @NotNull String username;
+    private final @Nullable String password;
+
+    private final @NotNull InetAddress hostname;
+
+    @Range(from = 0, to = 65535)
+    private final int port;
+
+    @ApiStatus.Internal
+    private @Nullable Connection connection;
+
+    protected @Nullable ScheduledExecutorService keepAliveExecutor;
+
+    public MysqlAuthentication(@NotNull String username, @Nullable String password, @NotNull InetAddress hostname, @Range(from = 0, to = 65535) int port) {
+        this.username = username;
+        this.password = password;
+        this.hostname = hostname;
+        this.port = port;
+    }
+
+    /**
+     * Retrieves te connection of this authentication
+     * @return The authentication connection or null if isn't authenticated
+     */
+    public final @Nullable Connection getConnection() {
+        return connection;
+    }
+    public final boolean isConnected() {
+        return connection != null;
+    }
+
+    @Contract(pure = true)
+    public final @NotNull String getUsername() {
+        return username;
+    }
+
+    @Contract(pure = true)
+    public final @Nullable String getPassword() {
+        return password;
+    }
+
+    @Contract(pure = true)
+    public final @NotNull InetAddress getHostname() {
+        return hostname;
+    }
+
+    @Contract(pure = true)
+    @Range(from = 0, to = 65535)
+    public final int getPort() {
+        return port;
+    }
+
+    /**
+     * Connects to the mysql with the provided authentication details.
+     *
+     * @return A CompletableFuture representing the asynchronous start operation
+     * @throws IllegalStateException If the database is already loaded
+     * @since 2.0
+     */
+    public final @NotNull CompletableFuture<Connection> connect() {
+        if (isConnected()) {
+            throw new IllegalStateException("This authentication already are connected!");
+        }
+
+        @NotNull CompletableFuture<Connection> future = new CompletableFuture<>();
+        // future.orTimeout(10, TimeUnit.SECONDS); // Not available in Java 8
+
+        CompletableFuture.runAsync(() -> {
+            try {
+                Class<Driver> driver = getDriver();
+                this.connection = load().get(10, TimeUnit.SECONDS);
+                future.complete(connection);
+            } catch (Throwable throwable) {
+                future.completeExceptionally(throwable);
+            }
+        });
+
+        return future;
+    }
+
+    public @NotNull Class<Driver> getDriver() {
+        try {
+            try {
+                //noinspection unchecked
+                return (Class<Driver>) Class.forName("com.mysql.cj.jdbc.Driver");
+            } catch (ClassNotFoundException e) {
+                //noinspection unchecked
+                return (Class<Driver>) Class.forName("com.mysql.jdbc.Driver");
+            }
+        } catch (Throwable e) {
+            throw new RuntimeException("Cannot load default driver 'com.mysql.jdbc.Driver'", e);
+        }
+    }
+
+    /**
+     * Stops the database.
+     *
+     * @return A CompletableFuture representing the asynchronous stop operation
+     * @throws IllegalStateException If the database is not loaded
+     * @since 2.0
+     */
+    public final @NotNull CompletableFuture<Void> disconnect() {
+        if (!isConnected()) {
+            throw new IllegalStateException("This authentication aren't connected");
+        }
+
+        @NotNull CompletableFuture<Void> future = new CompletableFuture<>();
+
+        CompletableFuture.runAsync(() -> {
+            try {
+                unload().get(10, TimeUnit.SECONDS);
+                connection = null;
+                future.complete(null);
+            } catch (Throwable throwable) {
+                future.completeExceptionally(throwable);
+            }
+        });
+
+        return future;
+    }
+
+    /**
+     * Loads the authentication, preparing it for operations and authenticating to the server.
+     *
+     * @return A CompletableFuture representing the asynchronous load operation
+     * @since 1.0
+     */
+    @ApiStatus.OverrideOnly
+    protected @NotNull CompletableFuture<Connection> load() {
+        @NotNull CompletableFuture<Connection> future = new CompletableFuture<>();
+        // future.orTimeout(10, TimeUnit.SECONDS); // Not available in Java 8
+
+        CompletableFuture.runAsync(() -> {
+            try {
+                @NotNull Connection connection = DriverManager.getConnection("jdbc:mysql://" + getHostname().getHostAddress() + ":" + getPort() + "/?autoReconnect=true&failOverReadOnly=false&verifyServerCertificate=false", getUsername(), getPassword());
+                connection.setNetworkTimeout(Executors.newFixedThreadPool(1), (int) TimeUnit.MINUTES.toMillis(30));
+
+                keepAliveExecutor = Executors.newScheduledThreadPool(1);
+                keepAliveExecutor.scheduleAtFixedRate(this::checkConnection, 0, 4, TimeUnit.MINUTES);
+
+                future.complete(connection);
+            } catch (Throwable throwable) {
+                future.completeExceptionally(throwable);
+            }
+        });
+
+        return future;
+    }
+
+    // TODO: 07/10/2023 Javadocs
+    private void checkConnection() {
+        // TODO: 07/10/2023 Connection check
+        System.out.println("Check connection");
+    }
+
+    /**
+     * Unloads the authentication, releasing resources.
+     *
+     * @return A CompletableFuture representing the asynchronous unload operation
+     * @since 1.0
+     */
+    @ApiStatus.OverrideOnly
+    protected @NotNull CompletableFuture<Void> unload() throws SQLException {
+        if (keepAliveExecutor == null || connection == null) {
+            throw new IllegalStateException("The authentication hasn't started correctly");
+        } else if (connection.isClosed()) {
+            throw new IllegalStateException("The connection aren't open");
+        } else {
+            @NotNull CompletableFuture<Void> future = new CompletableFuture<>();
+
+            CompletableFuture.runAsync(() -> {
+                try {
+                    keepAliveExecutor.shutdown();
+                    keepAliveExecutor = null;
+
+                    if (!connection.getAutoCommit()) {
+                        connection.commit();
+                    }
+
+                    connection.close();
+                    future.complete(null);
+                } catch (Throwable throwable) {
+                    future.completeExceptionally(throwable);
+                }
+            });
+
+            return future;
+        }
+    }
+
+}
