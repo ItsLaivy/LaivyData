574ee912b9d5465b5c52577382507e197c548141
Laivy <dnlfg.contato@gmail.com>
2023-11-01 18:28:12 -0300

    Added abstract type

---
+++ mysql/src/main/java/codes/laivy/data/mysql/variable/type/AbstractType.java
@@ -0,0 +1,52 @@
+package codes.laivy.data.mysql.variable.type;
+
+import codes.laivy.data.mysql.variable.MysqlVariable;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.concurrent.CompletableFuture;
+
+public abstract class AbstractType<T> implements Type<T> {
+
+    private final @NotNull String sqlName;
+
+    public AbstractType(@NotNull String sqlName) {
+        this.sqlName = sqlName;
+    }
+
+    @Contract(pure = true)
+    public final @NotNull String getSqlName() {
+        return sqlName;
+    }
+
+    @Override
+    public final @NotNull CompletableFuture<Void> configure(@NotNull MysqlVariable<T> variable) {
+        @Nullable Connection connection = variable.getDatabase().getAuthentication().getConnection();
+        if (connection == null) {
+            throw new IllegalStateException("The variable's authentication aren't connected");
+        }
+
+        @NotNull CompletableFuture<Void> future = new CompletableFuture<>();
+
+        CompletableFuture.runAsync(() -> {
+            try {
+                boolean exists = variable.exists().join();
+
+                if (!exists) {
+                    try (@NotNull PreparedStatement statement = connection.prepareStatement("ALTER TABLE `" + variable.getDatabase().getId() + "`.`" + variable.getTable().getName() + "` ADD COLUMN `" + variable.getId() + "` " + getSqlName() + ";")) {
+                        statement.execute();
+                    }
+                }
+
+                future.complete(null);
+            } catch (@NotNull Throwable throwable) {
+                future.completeExceptionally(throwable);
+            }
+        });
+
+        return future;
+    }
+}
