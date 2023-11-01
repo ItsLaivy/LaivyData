a3adc546234df2cbf74c14bc79235b806f5f9907
Laivy <dnlfg.contato@gmail.com>
2023-11-01 13:49:25 -0300

    Improved data system

--- mysql/src/main/java/codes/laivy/data/mysql/data/MysqlData.java
+++ mysql/src/main/java/codes/laivy/data/mysql/data/MysqlData.java
@@ -2,27 +2,31 @@ package codes.laivy.data.mysql.data;
 
 import codes.laivy.data.data.Data;
 import codes.laivy.data.mysql.MysqlDatabase;
-import codes.laivy.data.mysql.MysqlTable;
+import codes.laivy.data.mysql.table.MysqlTable;
 import codes.laivy.data.mysql.utils.SqlUtils;
+import codes.laivy.data.mysql.variable.Parameter;
+import codes.laivy.data.mysql.variable.Type;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.*;
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 
-public abstract class MysqlData extends Data {
+public final class MysqlData extends Data {
 
     // Static methods
 
     private static final @NotNull Map<@NotNull MysqlTable, @NotNull List<@NotNull MysqlData>> CACHED_LOADED_DATAS = new HashMap<>();
 
-    public static @NotNull CompletableFuture<Data> retrieve(@NotNull MysqlTable table, long row) {
-
-    }
+    // TODO: 01/11/2023 This
+//    public static @NotNull CompletableFuture<Data> retrieve(@NotNull MysqlTable table, long row) {
+//
+//    }
     public static @NotNull CompletableFuture<Data[]> retrieve(@NotNull MysqlTable table, @NotNull Condition<?> @NotNull ... conditions) {
         @Nullable Connection connection = table.getDatabase().getAuthentication().getConnection();
 
@@ -52,27 +56,27 @@ public abstract class MysqlData extends Data {
                     }
                 }
 
-                // Query
-
                 // Retrieving on database
 
-                try (PreparedStatement statement = connection.prepareStatement("SELECT * FROM `" + table.getDatabase().getId() + "`.`" + table.getName() + "` " + SqlUtils.buildWhereCondition(datas.keySet(), conditions))) {
+                try (PreparedStatement statement = connection.prepareStatement("SELECT `row` FROM `" + table.getDatabase().getId() + "`.`" + table.getName() + "` " + SqlUtils.buildWhereCondition(datas.keySet(), conditions))) {
+                    int index = 0;
+                    for (@NotNull Condition<?> condition : conditions) {
+                        //noinspection rawtypes
+                        @NotNull Type type = condition.getVariable().getType();
+                        //noinspection unchecked
+                        type.set(Parameter.of(statement, type.isNullSupported(), index), condition.getValue());
+                        index++;
+                    }
 
-                    long row = 0;
-                    @NotNull MysqlData data = new MysqlData(table) {
-                        @Override
-                        protected @NotNull CompletableFuture<Void> load() {
-                            return null;
+                    @NotNull ResultSet set = statement.executeQuery();
+                    while (set.next()) {
+                        long row = set.getInt("row");
+                        @NotNull MysqlData data = new MysqlData(table, row);
+                        CACHED_LOADED_DATAS.computeIfAbsent(table, k -> new ArrayList<>()).add(data);
+
+                        if (!datas.containsKey(row)) {
+                            datas.put(row, data);
                         }
-
-                        @Override
-                        protected @NotNull CompletableFuture<Void> unload(boolean save) {
-                            return null;
-                        }
-                    };
-
-                    if (!datas.containsKey(row)) {
-                        datas.put(row, data);
                     }
                 }
 
@@ -82,7 +86,6 @@ public abstract class MysqlData extends Data {
             }
         });
 
-        CACHED_LOADED_DATAS.computeIfAbsent(table, k -> new ArrayList<>()).add(data);
         return future;
     }
 
@@ -91,8 +94,9 @@ public abstract class MysqlData extends Data {
     private final @NotNull MysqlTable table;
     private long row;
 
-    private MysqlData(@NotNull MysqlTable table) {
+    private MysqlData(@NotNull MysqlTable table, long row) {
         this.table = table;
+        this.row = row;
     }
 
     public final long getRow() {
@@ -112,7 +116,7 @@ public abstract class MysqlData extends Data {
     }
 
     @Override
-    public <T> @Nullable T get(@NotNull String id) {
+    public @Nullable Object get(@NotNull String id) {
         return null;
     }
 
@@ -122,12 +126,34 @@ public abstract class MysqlData extends Data {
     }
 
     @Override
+    protected @NotNull CompletableFuture<Void> load() {
+        return null;
+    }
+
+    @Override
+    protected @NotNull CompletableFuture<Void> unload(boolean save) {
+        return null;
+    }
+
+    @Override
     public @NotNull CompletableFuture<Void> save() {
         return null;
     }
 
     public boolean matches(@NotNull Condition<?> @NotNull [] conditions) {
+        if (!isLoaded()) {
+            throw new IllegalStateException("The mysql data must be loaded to use the #matches");
+        } else if (Arrays.stream(conditions).anyMatch(c -> !c.getVariable().getTable().equals(getTable()))) {
+            throw new IllegalStateException("There's conditions with variables that aren't from this data");
+        } else if (Arrays.stream(conditions).anyMatch(c -> !c.getVariable().isLoaded())) {
+            throw new IllegalStateException("There's conditions with variables that hasn't loaded");
+        } else for (@NotNull Condition<?> condition : conditions) {
+            if (!Objects.equals(get(condition.getVariable().getId()), condition.getValue())) {
+                return false;
+            }
+        }
 
+        return true;
     }
 
 }
